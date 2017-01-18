eb8fe34a75780118b984142f65146b8583ac55ea
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-01-18 09:42:29 +0100

    pep8 corrections

--- burials/models.py
+++ burials/models.py
@@ -194,22 +194,24 @@ class Burial(models.Model):
 
 
 class UrnCover(models.Model):
-    cover_id = models.TextField(blank=True, null=True,           #ask if it is correct name for this property
-        help_text="helptext")
-    upside_down = models.CharField(max_length=5, blank=True,
-        null=True, choices=YESNO, help_text="helptext")
-    fragment = models.CharField(max_length=5, blank=True,
-        null=True, choices=YESNO, help_text="helptext")
-    basic_shape = models.ForeignKey(SkosConcept,
-        blank=True, null=True,
-        help_text="helptext", related_name = "skos_basic_shape_of_urn_cover")
-
+    cover_id = models.TextField(blank=True, null=True, help_text="helptext")
+    upside_down = models.CharField(
+        max_length=5, blank=True,
+        null=True, choices=YESNO, help_text="helptext"
+    )
+    fragment = models.CharField(
+        max_length=5, blank=True, null=True, choices=YESNO, help_text="helptext"
+    )
+    basic_shape = models.ForeignKey(
+        SkosConcept, blank=True, null=True,
+        help_text="helptext", related_name="skos_basic_shape_of_urn_cover"
+    )
 
     def __str__(self):
         return "{}".format(self.id)
 
     def get_absolute_url(self):
-        return reverse('burials:urncover_detail', kwargs={'pk':self.id})
+        return reverse('burials:urncover_detail', kwargs={'pk': self.id})
 
     def get_classname(self):
         """Returns the name of the class as lowercase string"""
@@ -218,20 +220,18 @@ class UrnCover(models.Model):
 
 
 class Urn(models.Model):
-    burial = models.ForeignKey(Burial,
+    burial = models.ForeignKey(
+        Burial,
         blank=True, null=True,
         help_text="helptext")
-    basic_shape = models.ForeignKey(SkosConcept,
-        blank=True, null=True,
-        help_text="helptext", related_name = "skos_basic_shape_of_urn")
-    urn_id = models.TextField(blank=True, null=True,
-        help_text="helptext")
-    urn_type = models.TextField(blank=True, null=True,              #TYPE is reserved word in django, not clear TYPE OF WHAT ?
-        help_text="helptext")
-    variation = models.TextField(blank=True, null=True,
-        help_text="helptext")
-    cover = models.ForeignKey(UrnCover, blank=True,
-        null=True, help_text="helptext")
+    basic_shape = models.ForeignKey(
+        SkosConcept, blank=True, null=True,
+        help_text="helptext", related_name="skos_basic_shape_of_urn"
+    )
+    urn_id = models.TextField(blank=True, null=True, help_text="helptext")
+    urn_type = models.TextField(blank=True, null=True, help_text="helptext")
+    variation = models.TextField(blank=True, null=True, help_text="helptext")
+    cover = models.ForeignKey(UrnCover, blank=True, null=True, help_text="helptext")
     # position_of_cremated_remains = models.ForeignKey(SkosConcept,
  #        blank=True, null=True,
  #        help_text="helptext", related_name = "skos_position_of_cremated_remains")
@@ -246,7 +246,7 @@ class Urn(models.Model):
         return "{}".format(self.id)
 
     def get_absolute_url(self):
-        return reverse('burials:urn_detail', kwargs={'pk':self.id})
+        return reverse('burials:urn_detail', kwargs={'pk': self.id})
 
     def get_classname(self):
         """Returns the name of the class as lowercase string"""
@@ -255,30 +255,27 @@ class Urn(models.Model):
 
 
 class GraveGood(models.Model):
-    burial = models.ForeignKey(Burial,
+    burial = models.ForeignKey(Burial, blank=True, null=True, help_text="helptext")
+    name = models.ForeignKey(
+        SkosConcept, blank=True, null=True, help_text="helptext",
+        related_name="skos_name_gravegood")
+    material = models.ForeignKey(
+        SkosConcept,
         blank=True, null=True,
-        help_text="helptext")
-    name = models.ForeignKey(SkosConcept,
-        blank=True, null=True,
-        help_text="helptext", related_name = "skos_name_gravegood")
-    material = models.ForeignKey(SkosConcept,
-        blank=True, null=True,
-        help_text="helptext", related_name = "skos_material")
-    condition = models.ForeignKey(SkosConcept,
-        blank=True, null=True,
-        help_text="helptext", related_name = "skos_condition")
-    position = models.ForeignKey(SkosConcept,
-        blank=True, null=True,
-        help_text="helptext", related_name = "skos_position")
-    amount = models.IntegerField(null=True, blank=True,
-        help_text="helptext")
-
+        help_text="helptext", related_name="skos_material")
+    condition = models.ForeignKey(
+        SkosConcept, blank=True, null=True,
+        help_text="helptext", related_name="skos_condition")
+    position = models.ForeignKey(
+        SkosConcept, blank=True, null=True,
+        help_text="helptext", related_name="skos_position")
+    amount = models.IntegerField(null=True, blank=True, help_text="helptext")
 
     def __str__(self):
         return "{}".format(self.id)
 
     def get_absolute_url(self):
-        return reverse('burials:gravegood_detail', kwargs={'pk':self.id})
+        return reverse('burials:gravegood_detail', kwargs={'pk': self.id})
 
     def get_classname(self):
         """Returns the name of the class as lowercase string"""
@@ -287,22 +284,23 @@ class GraveGood(models.Model):
 
 
 class GraveGoodOther(models.Model):
-    burial = models.ForeignKey(Burial,
-        blank=True, null=True,
+    burial = models.ForeignKey(
+        Burial, blank=True, null=True,
         help_text="helptext")
-    food = models.CharField(max_length=5, blank=True,
+    food = models.CharField(
+        max_length=5, blank=True,
         null=True, choices=YESNO, help_text="helptext")
-    other_organic_grave_good = models.CharField(max_length=5, blank=True,
+    other_organic_grave_good = models.CharField(
+        max_length=5, blank=True,
         null=True, choices=YESNO, help_text="helptext")
-    other_organic_grave_good_text = models.TextField(blank=True, null=True,
-        help_text="helptext")
-
+    other_organic_grave_good_text = models.TextField(
+        blank=True, null=True, help_text="helptext")
 
     def __str__(self):
         return "{}".format(self.id)
 
     def get_absolute_url(self):
-        return reverse('burials:gravegoodother_detail', kwargs={'pk':self.id})
+        return reverse('burials:gravegoodother_detail', kwargs={'pk': self.id})
 
     def get_classname(self):
         """Returns the name of the class as lowercase string"""
@@ -311,24 +309,20 @@ class GraveGoodOther(models.Model):
 
 
 class DeadBodyRemains(models.Model):
-    burial = models.ForeignKey(Burial,
-        blank=True, null=True,
-        help_text="helptext")
-    age = models.ForeignKey(SkosConcept,
-        blank=True, null=True,
-        help_text="helptext", related_name="skos_age")
-    gender = models.ForeignKey(SkosConcept,
-        blank=True, null=True,
-        help_text="helptext", related_name="skos_gender")
-    temperature = models.ForeignKey(SkosConcept,
-        blank=True, null=True,
+    burial = models.ForeignKey(
+        Burial, blank=True, null=True, help_text="helptext")
+    age = models.ForeignKey(
+        SkosConcept, blank=True, null=True, help_text="helptext", related_name="skos_age")
+    gender = models.ForeignKey(
+        SkosConcept, blank=True, null=True, help_text="helptext", related_name="skos_gender")
+    temperature = models.ForeignKey(
+        SkosConcept, blank=True, null=True,
         help_text="helptext", related_name="skos_temperature")
-    weight = models.TextField(blank=True, null=True,
-        help_text="helptext")
-    pathology = models.TextField(blank=True, null=True,
-        help_text="helptext")
-    total_weight = models.TextField(blank=True, null=True,
-        help_text="helptext")
+    weight = models.TextField(blank=True, null=True, help_text="helptext")
+    pathology = models.TextField(
+        blank=True, null=True, help_text="helptext")
+    total_weight = models.TextField(
+        blank=True, null=True, help_text="helptext")
 
     def __str__(self):
         return "{}".format(self.id)
