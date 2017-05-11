aff92ecede345391c24adc7a83403c1f0a36e9e3
Alexander Watzinger <alexander.watzinger@craws.net>
2017-05-11 13:10:44 +0200

    added ACDH favicon

--- webpage/urls.py
+++ webpage/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 from . import views
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     url(r'^$', views.StartView.as_view(), name="start"),
+    url(r'^favicon\.ico$', favicon_view),
     url(r'^imprint/$', views.ImprintView.as_view(), name='imprint'),
     url(r'^about/$', views.AboutView.as_view(), name='about'),
     url(r'^accounts/login/$', views.user_login, name='user_login'),
