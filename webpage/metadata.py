4f34d2b154d5dcf57e825ed052922fefced49f7d
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-10-04 16:44:59 +0200

    added "demo-version" to base.html

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -4,8 +4,8 @@
 PROJECT_METADATA = {
     'title': 'CBAB',
     'author': 'Peter Andorfer, Ksenia Zaytseva ',
-    'subtitle': 'Cremation Bronze Age Burials',
-    'description': 'text to come',
+    'subtitle': 'Cremation Bronze Age Burials - The Demo Version',
+    'description': 'The aim of Cremation Bronze Age Burials (CBAB) is the coordinated assessment of the cremation burials in the Late Bronze Age (14th – 9th century BC) based on a shared database.',
     'github': 'https://github.com/acdh-oeaw/cbab',
     'purpose_de': 'Das Ziel des Projekts "CBAB" ist die Publikation von Daten über Brandgräberfeldern.',
     'purpose_en': 'The purpose of the project "CBAB" is the publication of data about Cremation Bronze Age Burials.'}
