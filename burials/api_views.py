97d13c84070679d8f001caf754e9bac3fbe72a8d
zxenia <ksenia1zaytseva@gmail.com>
2017-12-19 10:10:41 +0100

    added pagination

--- burials/api_views.py
+++ burials/api_views.py
@@ -3,7 +3,6 @@ from rest_framework import pagination
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import *
 from .serializers import *
-from rest_framework.settings import api_settings
 
 
 class LargeResultsSetPagination(pagination.PageNumberPagination):
@@ -15,43 +14,52 @@ class LargeResultsSetPagination(pagination.PageNumberPagination):
 class BurialSiteViewSet(viewsets.ModelViewSet):
     queryset = BurialSite.objects.all()
     serializer_class = BurialSiteSerializer
+    pagination_class = LargeResultsSetPagination
 
 
 class BurialGroupViewSet(viewsets.ModelViewSet):
     queryset = BurialGroup.objects.all()
     serializer_class = BurialGroupSerializer
+    pagination_class = LargeResultsSetPagination
 
 
 class BurialViewSet(viewsets.ModelViewSet):
     queryset = Burial.objects.all()
     serializer_class = BurialSerializer
+    pagination_class = LargeResultsSetPagination
 
 
 class UrnViewSet(viewsets.ModelViewSet):
     queryset = Urn.objects.all()
     serializer_class = UrnSerializer
+    pagination_class = LargeResultsSetPagination
 
 
 class UrnCoverViewSet(viewsets.ModelViewSet):
     queryset = UrnCover.objects.all()
     serializer_class = UrnCoverSerializer
+    pagination_class = LargeResultsSetPagination
 
 
 class GraveGoodViewSet(viewsets.ModelViewSet):
     queryset = GraveGood.objects.all()
     serializer_class = GraveGoodSerializer
+    pagination_class = LargeResultsSetPagination
 
 
 class GraveGoodOtherViewSet(viewsets.ModelViewSet):
     queryset = GraveGoodOther.objects.all()
     serializer_class = GraveGoodOtherSerializer
+    pagination_class = LargeResultsSetPagination
 
 
 class AnthropologyViewSet(viewsets.ModelViewSet):
     queryset = DeadBodyRemains.objects.all()
     serializer_class = DeadBodyRemainsSerializer
+    pagination_class = LargeResultsSetPagination
 
 
 class AnimalRemainsViewSet(viewsets.ModelViewSet):
     queryset = AnimalRemains.objects.all()
     serializer_class = AnimalRemainsSerializer
+    pagination_class = LargeResultsSetPagination
--- burials/serializers.py
+++ burials/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from .models import *
+from vocabs.serializers import *
 
 
 class BurialSiteSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = BurialSite
-        fields = '__all__'
+	class Meta:
+		model = BurialSite
+		fields = '__all__'
 
 
 class BurialGroupSerializer(serializers.HyperlinkedModelSerializer):
